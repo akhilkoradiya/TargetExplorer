5065f58a55413901f7c9863c1592d0a2c3a9d605
Akhil Koradiya <102510716+akhilkoradiya@users.noreply.github.com>
2022-09-14 19:39:24 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,20 @@
 #!/bin/bash  
 
-read -p "Enter the URL without http and https: " URL
+read -p "
+████████╗ █████╗ ██████╗  ██████╗ ███████╗████████╗    ███████╗██╗  ██╗██████╗ ██╗      ██████╗ ██████╗ ███████╗██████╗ 
+╚══██╔══╝██╔══██╗██╔══██╗██╔════╝ ██╔════╝╚══██╔══╝    ██╔════╝╚██╗██╔╝██╔══██╗██║     ██╔═══██╗██╔══██╗██╔════╝██╔══██╗
+   ██║   ███████║██████╔╝██║  ███╗█████╗     ██║       █████╗   ╚███╔╝ ██████╔╝██║     ██║   ██║██████╔╝█████╗  ██████╔╝
+   ██║   ██╔══██║██╔══██╗██║   ██║██╔══╝     ██║       ██╔══╝   ██╔██╗ ██╔═══╝ ██║     ██║   ██║██╔══██╗██╔══╝  ██╔══██╗
+   ██║   ██║  ██║██║  ██║╚██████╔╝███████╗   ██║       ███████╗██╔╝ ██╗██║     ███████╗╚██████╔╝██║  ██║███████╗██║  ██║
+   ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝ ╚══════╝   ╚═╝       ╚══════╝╚═╝  ╚═╝╚═╝     ╚══════╝ ╚═════╝ ╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝
+                                                                                                                        
+
+Enter the URL without http and https: " URL
 
 mkdir ./Output/$URL
 
-subfinder -d $URL -t 200 -v -o ./Output/$URL/allsub.txt
-amass enum  -ip -brute -d $URL -o ./Output/$URL/allsub1.txt
-bash ./.Script/crt.sh $URL > ./Output/$URL/allsub2.txt
-sort ./Output/$URL/allsub.txt ./Output/$URL/allsub1.txt ./Output/$URL/allsub2.txt | uniq -u > ./Output/$URL/finalsubdomain.txt
-cat ./Output/$URL/finalsubdomain.txt | httpx -threads 300 | tee -a ./Output/$URL/subdomain.txt
-cat ./Output/$URL/subdomain.txt | waybackurls | tee -a ./Output/$URL/waybackurls.txt
+subfinder -d $URL -t 200 -v -o ./Output/$URL/subfinder.txt
+amass enum  -ip -brute -d $URL -o ./Output/$URL/amass.txt
+bash ./.Script/crt.sh $URL > ./Output/$URL/crt_sh.txt
+sort ./Output/$URL/subfinder.txt ./Output/$URL/amass.txt ./Output/$URL/crt_sh.txt | uniq -u > ./Output/$URL/subdomain.txt
+cat ./Output/$URL/subdomain.txt | httpx -threads 300 | tee -a ./Output/$URL/subdomain_with_http.txt
