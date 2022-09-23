34cf903830071f364b961a3a1f061a7af37bb2ee
Akhil Koradiya <102510716+akhilkoradiya@users.noreply.github.com>
2022-09-23 15:02:32 +0530

    Update start.sh
    xss added

--- start.sh
+++ start.sh
@@ -11,10 +11,12 @@ read -p "
 
 Enter the URL without http and https: " URL
 
-mkdir ./Output/$URL
-
-subfinder -d $URL -t 200 -v -o ./Output/$URL/subfinder.txt
-amass enum  -ip -brute -d $URL -o ./Output/$URL/amass.txt
-bash ./.Script/crt.sh $URL > ./Output/$URL/crt_sh.txt
-sort ./Output/$URL/subfinder.txt ./Output/$URL/amass.txt ./Output/$URL/crt_sh.txt | uniq -u > ./Output/$URL/subdomain.txt
-cat ./Output/$URL/subdomain.txt | httpx -threads 300 | tee -a ./Output/$URL/subdomain_with_http.txt
+mkdir ./Output/$URL 
+cd ./Output/$URL && mkdir xss && cd ../.. 
+subfinder -d $URL -t 200 -v -o ./Output/$URL/allsub.txt
+amass enum  -ip -brute -d $URL -o ./Output/$URL/allsub1.txt
+bash ./.Script/crt.sh $URL > ./Output/$URL/allsub2.txt
+sort ./Output/$URL/allsub.txt ./Output/$URL/allsub1.txt ./Output/$URL/allsub2.txt | uniq -u > ./Output/$URL/finalsubdomain.txt
+httpx-toolkit -l  ./Output/$URL/finalsubdomain.txt | tee -a ./Output/$URL/subdomain.txt
+cat ./Output/$URL/subdomain.txt | waybackurls | tee -a ./Output/$URL/waybackurls.txt
+cat ./Output/$URL/waybackurls.txt | gf xss | sed 's/=.*/=/' | sed 's/URL: //' | sort -u | tee -a ./Output/$URL/xss/output.txt
